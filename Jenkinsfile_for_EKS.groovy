e20a767b9cc0da0fa4cb15d64ec1b7f50740862a
Huseyin Akten <husakten@gmail.com>
2020-02-08 13:31:13 -0600

    sdf

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -3,9 +3,14 @@ node {
             git 'https://github.com/hakten/terraform-aws-eks.git'
     }
         stage("stage2"){
+            sh "cd terraform-aws-eks"
             sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"
     }
         stage("stage3"){
+    
 
     }
         stage("stage4"){
