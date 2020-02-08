60fd3e3f597d0c5dd2041c65c7ebcc89964aeed9
Huseyin Akten <husakten@gmail.com>
2020-02-08 11:09:43 -0600

    build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,5 @@
 node {
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region.', name: 'AMI_REGION')])])
-    
     stage("Pull Repo"){
         git 'https://github.com/farrukh90/packer'
 
@@ -10,7 +9,6 @@ node {
         sh "packer build -var-file region=${AMI_REGION} tools/jenkins_example.json"
         
     }
-
     stage("Send Notification to Slack"){
         echo "hello"
         
