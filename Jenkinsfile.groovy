d87fde9b3e3ec0a58211cef137e3c89e33c12770
Huseyin Akten <husakten@gmail.com>
2020-02-08 10:43:16 -0600

    prop added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,19 +1,19 @@
 node {
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region.', name: 'AMI_REGION')])])
-        stage("Pull Repo"){
-            echo "hello"
+     stage("Pull Repo"){
+        echo "hello"
 
     }
-        stage("Build Image"){
-            sh "packer version"
+    stage("Build Image"){
+        sh "packer version"
         
     }
-        stage("Send Notification to Slack"){
-            echo "hello"
+    stage("Send Notification to Slack"){
+        echo "hello"
         
     }
-        stage("Send Email"){
-            echo "hello"
+    stage("Send Email"){
+        echo "hello"
         
     }
 }
