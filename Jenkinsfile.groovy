8eb785f29c21a4c3d808553adcc128bb012d6499
Huseyin Akten <husakten@gmail.com>
2020-02-08 11:05:15 -0600

    build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 node {
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region.', name: 'AMI_REGION')])])
     
-    ßstage("Pull Repo"){
+    stage("Pull Repo"){
         git 'https://github.com/farrukh90/packer'
 
     }
