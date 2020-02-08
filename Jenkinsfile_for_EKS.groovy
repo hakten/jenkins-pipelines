97543ff61975e433352d525d9441141578bdb773
Huseyin Akten <husakten@gmail.com>
2020-02-08 12:50:13 -0600

    init

---
+++ Jenkinsfile_for_EKS.groovy
@@ -0,0 +1,14 @@
+node {
+        stage("Pull Repo"){
+            git 'https://github.com/bkarakas/terraform-iaac-eks-burak.git'
+    }
+        stage("stage2"){
+
+    }
+        stage("stage3"){
+
+    }
+        stage("stage4"){
+
+    }
+}
