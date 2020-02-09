a8ccca057e860cfa8ddbaadccd36bfdc555c8943
Huseyin Akten <husakten@gmail.com>
2020-02-09 00:01:52 -0600

    sdfa

--- Jenkinsfile_for_packer.groovy
+++ Jenkinsfile_for_packer.groovy
@@ -29,7 +29,7 @@ node {
     }
     stage("Build Image"){
         sh "packer version"
-        //sh "packer  build -var region=${AMI_REGION} tools/${TOOL_TO_PROVISION}.json"
+        sh "packer  build -var region=${AMI_REGION} tools/${TOOL_TO_PROVISION}.json"
     }
     stage("Send Notification to Slack"){
         slackSend channel: 'nagios_alerts', message: "${TOOL_TO_PROVISION} has been built"
