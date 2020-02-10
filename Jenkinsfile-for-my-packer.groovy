78e4108916c0a2b33e47841429ee788bf796fe69
Huseyin Akten <husakten@gmail.com>
2020-02-09 23:08:10 -0600

    sadfa

--- Jenkinsfile-for-my-packer.groovy
+++ Jenkinsfile-for-my-packer.groovy
@@ -25,7 +25,7 @@ stage('Pull Repo') {
 }
 
 stage('Build Image') {
-    sh "packer build -var region=${REGION} instance_type="t2.micro" ssh_username="centos" tools/${TOOL-TO-PROVISION}.json"
+    sh "packer build -var region=${REGION} --var-file ansible-tower/variable.json tools/${TOOL-TO-PROVISION}.json"
 
 }
 
