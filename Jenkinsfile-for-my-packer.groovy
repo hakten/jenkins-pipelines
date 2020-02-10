1b07b18e46cd85b4ef5a0b51ec09f7a2f524a25d
Huseyin Akten <husakten@gmail.com>
2020-02-09 22:55:35 -0600

    esadf

--- Jenkinsfile-for-my-packer.groovy
+++ Jenkinsfile-for-my-packer.groovy
@@ -2,7 +2,7 @@ node {
 properties([
     parameters([
         choice(choices: ['ansible-tower', 'consul', 'elk', 'gitlab', 'golden_image', 'jenkins', 'nagiosxi', 'nexus', 'r1soft-server', 'vault'], 
-        description: 'Which tool\'s ami would you like to build?', 
+        description: 'Which tool ami would you like to build?', 
         name: 'TOOL-TO-PROVISION'), 
         choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], 
         description: 'Where would you like to build your AMI?', 
