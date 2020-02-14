496140e27681bb19aadde11905f29cfcf38814d0
Huseyin Akten <husakten@gmail.com>
2020-02-13 22:20:42 -0600

    asdfas

---
+++ Jenkinsfile_for_my_image_builder.groovy
@@ -0,0 +1,12 @@
+Node {
+    properties([[$class: 'JiraProjectProperty'], parameters([choice(choices: ['terraform0.11.14', 'terraform0.12.19'], description: 'Please choose the tool to build Docker image.', name: 'IMAGE')])])
+    stage("Pull Repo"){
+        ws ("tmp/"){
+            git 'https://github.com/hakten/Dockerfiles.git'
+        }
+        stage("Build Image") {
+            sh "cd ${IMAGE}"
+            sh "docker build -t ${IMAGE}"
+
+
+}
