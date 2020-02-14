e4f06476db62b51420cc6ecf06cf7a8995f1ee65
Huseyin Akten <husakten@gmail.com>
2020-02-13 22:29:32 -0600

    sdfadsf

--- Jenkinsfile_for_my_image_builder.groovy
+++ Jenkinsfile_for_my_image_builder.groovy
@@ -1,4 +1,4 @@
-Node {
+node {
     properties([[$class: 'JiraProjectProperty'], parameters([choice(choices: ['terraform0.11.14', 'terraform0.12.19'], description: 'Please choose the tool to build Docker image.', name: 'IMAGE')])])
     stage("Pull Repo"){
         ws ("tmp/"){
