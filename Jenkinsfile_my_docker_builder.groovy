299843094b33d174ad134a0efbecee8c8d4d8c85
Huseyin Akten <husakten@gmail.com>
2020-02-17 00:45:23 -0600

    sadf

--- Jenkinsfile_my_docker_builder.groovy
+++ Jenkinsfile_my_docker_builder.groovy
@@ -1,5 +1,7 @@
 node {
-    properties([[$class: 'JiraProjectProperty'], parameters([
+    properties([
+    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+    parameters([
         choice(choices: ['terraform', 'packer'], description: 'Please choose the tool to build Docker image.', name: 'TOOL'),
         string(defaultValue: '0.11.14', description: 'Please enter app version in numbers as in github folder, as 0.12.19', name: 'VERSION', trim: false)
     ])])
