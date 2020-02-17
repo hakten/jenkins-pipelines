48cebcf55d2edbffbb240672baef543b5eeca68e
Huseyin Akten <husakten@gmail.com>
2020-02-16 23:37:43 -0600

    docker builder added

--- Jenkinsfile_my_docker_builder.groovy
+++ Jenkinsfile_my_docker_builder.groovy
@@ -1,15 +1,22 @@
 node {
-    properties([[$class: 'JiraProjectProperty'], parameters([choice(choices: ['terraform0.11.14', 'terraform0.12.19'], description: 'Please choose the tool to build Docker image.', name: 'IMAGE')])])
+    properties([[$class: 'JiraProjectProperty'], parameters([choice(choices: ['terraform_0.11.14', 'terraform_0.12.19', 'packer_1.5.0'], description: 'Please choose the tool to build Docker image.', name: 'TOOL')])])
+    
     stage("Pull Repo"){
-        ws ("tmp/"){
             git 'https://github.com/hakten/Dockerfiles.git'
             sh "ls"
-        }
     }
+
     stage("Build Image") {
-        ws ("tmp/Dockerfiles"){ 
             sh "ls"
-            sh "docker build -t ${IMAGE} ${IMAGE}/."
-        }
+            sh "docker build -t ${TOOl} Dockerfiles/${TOOL}/."
+    }
+
+    stage("login to ECR"){
+            sh "$(aws ecr get-login --no-include-email --region eu-west-1)"
+    }
+
+    stage("push image"){
+            sh "docker images"
+            sh "docker push 103872286656.dkr.ecr.eu-west-1.amazonaws.com/${TOOL}"
     }
 }
