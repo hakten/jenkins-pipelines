73efe2cbf68a6db4847bf8fab9e128ffe128cf91
Huseyin Akten <husakten@gmail.com>
2020-02-16 18:23:28 -0600

    sdfsa

--- Jenkinsfile_my_artemis.groovy
+++ Jenkinsfile_my_artemis.groovy
@@ -45,16 +45,16 @@ node {
 				//slackSend color: '#BADA55', message: 'Hello, World!'
 			}
 
-    // stage("Send Email"){
-    //   mail 
-	//     bcc: '', 
-    //     body: "Hello, your artemis app is deployed to ${ENVIR}", 
-    //     cc: '', 
-    //     from: 'husakten@gmail.com', 
-    //     replyTo: '', 
-    //     subject: "Artemis ${Version} has been deployed", 
-    //     to: "${EMAIL_TO_SEND}"
-    // }
+    stage("Send Email"){
+      mail 
+	    bcc: '', 
+        body: "Hello, your artemis app is deployed to ${ENVIR}", 
+        cc: '', 
+        from: 'husakten@gmail.com', 
+        replyTo: '', 
+        subject: "Artemis ${Version} has been deployed", 
+        to: "${EMAIL_TO_SEND}"
+    }
 
 
 }
